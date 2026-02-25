e929f7be9e8ede46266049690ab23c68aa78f8ff
EmeraldWither <68785503+EmeraldWither@users.noreply.github.com>
2026-02-25 15:03:05 -0500

    add more motion test data

--- ros_ws/src/motion_node/motion_node/motion_node_test.py
+++ ros_ws/src/motion_node/motion_node/motion_node_test.py
@@ -3,7 +3,7 @@ from rclpy.executors import ExternalShutdownException
 from rclpy.node import Node
 
 from boat_common_libs.smooth_random import SmoothRandom
-from boat_data_interfaces.msg import (MotionData, BoatAlarm, GPSData, GPSVTGData, )
+from boat_data_interfaces.msg import (MotionData, BoatAlarm, GPSData, GPSVTGData, GPSSVData, )
                                       #GPSSpeed)  # type: ignore
 
 import random
@@ -14,6 +14,7 @@ class MotionNode(Node):
         super().__init__('motion_node_test')
         self._gps_pub = self.create_publisher(GPSData, '/motion/gps', 10)
         self._speed_pub = self.create_publisher(GPSVTGData, '/motion/vtg', 10)
+        self._sats_pub = self.create_publisher(GPSSVData, '/motion/sv', 10)
         timer_period = random.random() * 0.1
         self._logger.info("Sending test data at a period of " + str(timer_period))
 
@@ -22,15 +23,20 @@ class MotionNode(Node):
         self.gps_lat = SmoothRandom(start=40.744, step=0.0001, low=40.73, high=40.76)
         self.gps_long = SmoothRandom(start=-74.032, step=0.0001, low=-74.06, high=-74.02)
         self.speed = SmoothRandom(start=5, step=0.5, low=-10, high=40)
+        self.heading = SmoothRandom(start=55, step=1, low=0, high=360)
+        self.sats = SmoothRandom(start=6, step=0.5, low=0, high=18)
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
     def timer_callback(self):
         msg = GPSData()
+        sats = GPSSVData()
 
         msg.lon = self.gps_long.next()
         msg.lat = self.gps_lat.next()
         self._gps_pub.publish(msg)
-        self._speed_pub.publish(GPSVTGData(speed=float(self.speed.next()), true_track=float(14)))
+        self._speed_pub.publish(GPSVTGData(speed=float(self.speed.next()), true_track=float(self.heading.next())))
+        sats.sats = int(self.sats.next())
+        self._sats_pub.publish(sats)
 
 
 def main(args=None):
