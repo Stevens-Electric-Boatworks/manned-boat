cb3ac468f441c924487d050e39969eeb32149137
Ishaan <68785503+EmeraldWither@users.noreply.github.com>
2026-04-12 11:24:52 -0400

    fix uniterable error

--- ros_ws/src/can_node/can_node/can_node.py
+++ ros_ws/src/can_node/can_node/can_node.py
@@ -97,7 +97,7 @@ class MotorNode(Node):
         self.unlatchedMotorAlarms.append((is_motor_a, eventId))
 
     def unlatch_alarm(self, alarm: Alarm):
-        self.unlatchedMotorAlarms.append(alarm)
+        self.alarms.append(alarm)
 
     def declare_alarm(self, alarm: Alarm):
         self.alarms.append(alarm)
