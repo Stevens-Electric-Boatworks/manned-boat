9d67ee4ca4be84fb47936345614e5ae7d111d3ce
Ishaan <68785503+EmeraldWither@users.noreply.github.com>
2026-05-22 17:29:37 -0400

    add deploy script

--- ros_ws/deploy_cpp.sh
+++ ros_ws/deploy_cpp.sh
@@ -19,13 +19,26 @@ echo "Building nodes (CPP)"
 
 echo "Searching for MiniPC"
 
+verify_login() {
+ sshpass -p "$EBOAT_PASSWORD" ssh -o StrictHostKeyChecking=no -o ConnectTimeout=5 eboat@"$1" exit
+  if [ $? -eq 0 ]; then
+    echo "Credentials Validated"
+  else
+    echo "Wrong credientials!"
+    exit 1
+  fi
+}
+
 deploy_code_to_pc () {
   echo "Deploying Code to MiniPC"
-  if ! sshpass -p "$EBOAT_PASSWORD" rsync -a build eboat@"$1":~/eboat_src/ros_ws/build; then
+  verify_login $1
+
+  if ! sshpass -p "$EBOAT_PASSWORD" rsync --exclude={".vscode",".idea","log","build"} --delete -aLzPh . eboat@"$1":~/eboat_src/ros_ws/; then
     echo -e "\e[31m[ERR] Unable to deploy to MiniPC\e[0m"
     exit 1
   fi
   echo -e "\e[32mSuccess!\e[0m"
+  exit 0
 }
 
 FOUND_IP=false
@@ -40,8 +53,12 @@ try_ip() {
   fi
 }
 
-try_ip "mini-pc.local"
-try_ip "localhost"
+#build code
+./cpp_deploy_build.sh
+
+try_ip "eboat.local"
+try_ip "10.3.141.1"
+try_ip "192.168.55.1"
 
 # TailScale IP
 try_ip "eboat-nuc.tail617ae5.ts.net"
